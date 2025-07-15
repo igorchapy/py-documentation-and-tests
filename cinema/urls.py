0afabb8168c562905f1438e9569b61b5c996151b
Igor Chapy <misha@mate.acedemy>
2025-07-15 14:01:14 +0300

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,18 +1,13 @@
-from django.urls import path
-from user.views import CreateUserView, CreateTokenView, ManageUserView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from cinema.views import MovieViewSet
 
-app_name = "user"
+
+app_name = "cinema"
+
+router = DefaultRouter()
+router.register("movies", MovieViewSet, basename="movie")  # basename дуже важливий
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
-    path("me/", ManageUserView.as_view(), name="manage"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("", include(router.urls)),
 ]
